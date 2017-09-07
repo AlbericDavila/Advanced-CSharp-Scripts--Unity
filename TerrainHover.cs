d14379cf6744551348230f7070b54f76ad93fedd
Alberic A. Davila Cadilla <alberic.davila@gmail.com>
2017-09-07 13:26:00 -0300

    Update TerrainHover.cs

--- TerrainHover.cs
+++ TerrainHover.cs
@@ -1,28 +1,28 @@
-﻿using UnityEngine;
+using UnityEngine;
 using System.Collections;
 using UnityStandardAssets.CrossPlatformInput;
 
 public class TerrainHover : MonoBehaviour 
 {
-	private Transform thisTransform;
+    private Transform thisTransform;
     private Vector3 destinationUpVector = Vector3.zero;
     private Vector3 newPosition;
     private float horizontalMovementInput;  // Ranges from 1 to 0
     private float verticalMovementInput;    // Ranges from 1 to 0
     public float maxSpeed = 10f;
-	public float distanceFromGround = 2f;
-	public float angleSpeed = 5f;           // Speed of the change of angles of this transform
+    public float distanceFromGround = 2f;
+    public float angleSpeed = 5f;           // Speed of the change of angles of this transform
     public float rotationSpeed = 90f;       // Speed of the change of rotation of this transform
 
     // Use this for initialization
     void Awake () 
-	{
-		thisTransform = GetComponent<Transform>();
-	}
+    {
+	thisTransform = GetComponent<Transform>();
+    }
 	
-	// Update is called once per frame
-	void Update () 
-	{
+    // Update is called once per frame
+    void Update () 
+    {
 		horizontalMovementInput = CrossPlatformInputManager.GetAxis("Horizontal");
 		verticalMovementInput = CrossPlatformInputManager.GetAxis("Vertical");
 
