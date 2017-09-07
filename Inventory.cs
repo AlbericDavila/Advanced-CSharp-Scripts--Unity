eadeca1d9045cb3cff7fc060fd92f60ea0891672
Alberic A. Davila Cadilla <alberic.davila@gmail.com>
2017-09-07 11:12:30 -0400

    Add files via upload

---
+++ Inventory.cs
@@ -0,0 +1,61 @@
+﻿
+using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+
+public class Inventory : MonoBehaviour
+{
+    /// <summary>
+    /// Property for accessing single instance, there should only be 1 instance of the inventory at all times.
+    /// This allows us to set it to static to be used accros all scripts without a need to reference it.
+    /// </summary>
+    public static Inventory Instance
+	{
+		get
+		{
+			if(thisInstance==null)
+			{
+				GameObject inventoryObject = new GameObject("Inventory");
+				thisInstance = inventoryObject.AddComponent<Inventory>();
+			}
+
+			return thisInstance;
+		}
+	}
+
+	private static Inventory thisInstance = null;   // Reference to singleton object
+    public RectTransform itemList = null;           // Root object of item list
+
+    void Awake ()
+	{
+		// If single object already exists then destroy
+		if(thisInstance!=null)
+		{
+			DestroyImmediate(gameObject);
+			return;
+		}
+
+		// Make this single instance
+		thisInstance = this;
+	}
+
+    public static void addItem(GameObject GO)
+	{
+        // Destroy picked up item
+        Destroy(GO.gameObject);
+
+		// Add to first available slot
+		for(int i=0; i<thisInstance.itemList.childCount; i++)
+		{
+			Transform item = thisInstance.itemList.GetChild(i);
+
+			// If Item is not active, then it becomes new slot
+			if(!item.gameObject.activeSelf)
+			{
+				item.GetComponent<Image>().sprite = GO.GetComponent<InventoryItem>().GUI_Icon;
+				item.gameObject.SetActive(true);
+				return;
+			}
+		}
+	}
+}
---
+++ InventoryItem.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+
+/// <summary>
+/// This script goes attached to the gameObject you wish to make an item
+/// </summary>
+public class InventoryItem : MonoBehaviour
+{
+	// This enum can be scaled to fit any type of item (e.g. Food, Weapon, Clothing, etc.)
+	public enum ITEMTYPE {SPHERE, BOX, CYLINDER};
+	public ITEMTYPE Type;
+	public Sprite GUI_Icon;
+	
+	void OnTriggerEnter(Collider collingObject)
+	{
+        // If item collides with anything that is not the player, then return.
+		if(!collingObject.CompareTag("Player"))
+			return;
+
+		// Add this item to the inventory
+		Inventory.addItem(gameObject);
+	}
+}
